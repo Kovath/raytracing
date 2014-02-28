fbc75e0c521364f2aeb5ff3f6968f8be2b6063b7
Darren Tsung <darren.tsung@gmail.com>
2014-02-28 13:49:13 -0800

    get and set for triangle

--- src/triangle.h
+++ src/triangle.h
@@ -8,17 +8,28 @@ using Eigen::Vector3f;
 
 class Triangle : public virtual Primitive {
     public:
-        // a triangle is defined by three points
-        // these MUST be passed in countclockwise order in order for
-        // one-sided triangles to exist
-        Vector3f p0, p1, p2;
-
         // constructors
         Triangle() {}
         Triangle(Vector3f p0, Vector3f p1, Vector3f p2): p0(p0), p1(p1), p2(p2) {}
 
         // sets the intersection time to float pointer t if ray hits object
         bool did_ray_hit(Ray ray, float *intersection_t);
+
+        // setter functions
+        void set_p0(Vector3f p);
+        void set_p1(Vector3f p);
+        void set_p2(Vector3f p);
+
+        // getter functions
+        Vector3f get_p0();
+        Vector3f get_p1();
+        Vector3f get_p2();
+
+    private:
+        // a triangle is defined by three points
+        // these MUST be passed in countclockwise order in order for
+        // one-sided triangles to exist
+        Vector3f p0, p1, p2;
 };
 
 #endif // TRIANGLE_H
